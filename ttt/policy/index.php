d015835717c0d8f1eec3ceaf82032ed317ef8a86
vtweb3 <openshift.vttech@gmail.com>
2020-01-12 18:41:51 -0600

    adding privacy policy

---
+++ ttt/policy/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+	<title>AnDaLaDa Policy</title>
+	<meta name="generator" content="LibreOffice 6.2.8.2 (MacOSX)"/>
+	<meta name="author" content="user one"/>
+	<meta name="created" content="2012-02-19T00:00:00.020075400"/>
+	<meta name="changed" content="2020-01-12T14:58:42.241056518"/>
+	<style type="text/css">
+		@page { margin: 0.79in }
+		p { margin-bottom: 0.08in }
+		a:link { color: #000000 }
+	</style>
+</head>
+<body lang="en-US" link="#000000" dir="ltr"><p align="center" style="margin-bottom: 0in">
+<font color="#0000ff"><font face="Noteworthy"><font size="5" style="font-size: 18pt"><b>Privacy
+Policy</b></font></font></font></p>
+<p align="center" style="margin-bottom: 0in"><br/>
+
+</p>
+<p style="margin-bottom: 0in"><font color="#000000"><font face="Helvetica, sans-serif"><font size="3" style="font-size: 12pt">Last
+Updated/Effective Date January 1, 2020</font></font></font></p>
+<p style="margin-bottom: 0in"><br/>
+
+</p>
+<p style="margin-bottom: 0in"><font color="#000000"><font face="Helvetica, sans-serif"><font size="3" style="font-size: 12pt">We
+at Tic Tac Toe For Chomecast does not collect/retain user
+usage/information/data.   </font></font></font>
+</p>
+<p style="margin-bottom: 0in"><br/>
+
+</p>
+<p style="margin-bottom: 0in"><font color="#000000"><font face="Helvetica, sans-serif"><font size="3" style="font-size: 12pt">However,
+your Chromecast device (Google) may collect system activity, crash
+reports, and usage data.  For more information and on how to control
+this activity on your Chromecast cast hardware, please see the <a href="https://support.google.com/chromecast/answer/6076570?hl=en">Google
+Chromecast Privacy Settings</a>. </font></font></font>
+</p>
+<p style="margin-bottom: 0in"><br/>
+
+</p>
+<p style="margin-bottom: 0in"><br/>
+
+</p>
+<p style="margin-bottom: 0in"><br/>
+
+</p>
+</body>
+</html>
